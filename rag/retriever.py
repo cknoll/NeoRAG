8605865c32751cd1a9f910b847d8f614fcd2feb4
Carsten Knoll <carsten.knoll@posteo.de>
2026-03-31 20:35:47 +0200

    manual fixing bugs

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,6 +1,7 @@
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.retrievers import VectorIndexRetriever
-from llama_index.postprocessor.sentence_transformers_rerank import SentenceTransformerRerank
+# from llama_index.postprocessor.sentence_transformers_rerank import SentenceTransformerRerank
+from llama_index.core.postprocessor import SentenceTransformerRerank
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from .config import QDRANT_PATH, COLLECTION_NAME, EMBEDDING_MODEL, TOP_K_BASE, TOP_K_FINAL, RERANK_MODEL
 from qdrant_client import QdrantClient
@@ -14,30 +15,30 @@ def get_query_engine():
         client=client,
         collection_name=COLLECTION_NAME
     )
-    
+
     embed_model = HuggingFaceEmbedding(model_name=EMBEDDING_MODEL)
-    
+
     index = VectorStoreIndex.from_vector_store(
         vector_store=vector_store,
         embed_model=embed_model
     )
-    
+
     # Base retriever: get many candidates
     base_retriever = VectorIndexRetriever(
         index=index,
         similarity_top_k=TOP_K_BASE
     )
-    
+
     # Re-ranker: crucial for accuracy
     reranker = SentenceTransformerRerank(
         model=RERANK_MODEL,
         top_n=TOP_K_FINAL
     )
-    
+
     # Assemble query engine
     query_engine = RetrieverQueryEngine.from_args(
         retriever=base_retriever,
         node_postprocessors=[reranker]
     )
-    
+
     return query_engine
